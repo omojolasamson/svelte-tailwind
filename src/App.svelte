6eea060888c24ab580d19699c6843deda70ae286
omojolasamson <sammy_omojola@yahoo.com>
2021-02-23 09:20:24 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+</script>
+
+<main>
+	<nav class="bg-blue-900 shadow-lg">
+		<div class="container mx-auto">
+		 <div class="sm:flex">
+		  <a href="#" class="text-white text-3xl font-bold p-3">
+			  APP LOGO</a>
+			 
+		  <!-- Menus  -->
+		  <div class="ml-55 mt-4">
+		   <ul class="text-white sm:self-center text-xl">
+			   <li class="sm:inline-block">
+				   <a href="#" class="p-3 hover:text-red-900">About</a>
+			   </li>
+			   <li class="sm:inline-block">
+				   <a href="#" class="p-3 hover:text-red-900">Services</a>
+			   </li>
+			   <li class="sm:inline-block">
+				   <a href="#" class="p-3 hover:text-red-900">Blog</a>
+			   </li>
+			   <li class="sm:inline-block">
+				   <a href="#" class="p-3 hover:text-red-900">Contact</a>
+			   </li>
+		   </ul>
+		  </div>
+		 </div>
+		</div>
+	</nav>
+
+</main>
+
+<style global lang="postcss">
+	@tailwind base;
+  	@tailwind components;
+  /* purgecss end ignore */
+  	@tailwind utilities;
+
+
+	@layer utilities {
+  		.ml-55 {
+    		margin-left: 55%;
+		}
+	}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const production = !process.env.ROLLUP_WATCH; 
+module.exports = {
+  future: { 
+    purgeLayersByDefault: true, 
+    removeDeprecatedGapUtilities: true,
+  },
+  plugins: [
+
+  ],
+  purge: {
+    content: [
+      "./src/**/*.svelte",
+      
+    ], 
+    enabled: production // disable purge in dev
+  },
+};
