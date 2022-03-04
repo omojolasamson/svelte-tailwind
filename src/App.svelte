bc4ec991ecbf556d1b59bbcf4f04314991cccfe8
Omojola Samson <sammy_omojola@yahoo.com>
2022-03-04 12:47:10 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -5,23 +5,23 @@
 	<nav class="bg-blue-900 shadow-lg">
 		<div class="container mx-auto">
 		 <div class="sm:flex">
-		  <a href="#" class="text-white text-3xl font-bold p-3">
+		  <a href="#/" class="text-white text-3xl font-bold p-3">
 			  APP LOGO</a>
 			 
 		  <!-- Menus  -->
 		  <div class="ml-55 mt-4">
 		   <ul class="text-white sm:self-center text-xl">
 			   <li class="sm:inline-block">
-				   <a href="#" class="p-3 hover:text-red-900">About</a>
+				   <a href="#/" class="p-3 hover:text-red-900">About</a>
 			   </li>
 			   <li class="sm:inline-block">
-				   <a href="#" class="p-3 hover:text-red-900">Services</a>
+				   <a href="#/" class="p-3 hover:text-red-900">Services</a>
 			   </li>
 			   <li class="sm:inline-block">
-				   <a href="#" class="p-3 hover:text-red-900">Blog</a>
+				   <a href="#/" class="p-3 hover:text-red-900">Blog</a>
 			   </li>
 			   <li class="sm:inline-block">
-				   <a href="#" class="p-3 hover:text-red-900">Contact</a>
+				   <a href="#/" class="p-3 hover:text-red-900">Contact</a>
 			   </li>
 		   </ul>
 		  </div>
